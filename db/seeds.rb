cd4b59bce78a9fd031ef0460497ae2dd73e538cc
Juliana Arrighi <jarrighi@poli.ufrj.br>
2022-02-22 18:56:22 -0300

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "------------ Seeding... --------------"
+restaurant1 = Restaurant.create(name: "Vila Rica", address: "Rua Candido Mendes esquina com rua da Glória", category: "french", phone_number: "2124-6722")
+restaurant1 = Restaurant.create(name: "Amarelinho", address: "Rua da Glória, 5", category: "italian", phone_number: "2124-5620")
+restaurant1 = Restaurant.create(name: "Panka", address: "Rua Armando Lombardi, 156", category: "chinese", phone_number: "2704-6132")
+restaurant1 = Restaurant.create(name: "Ferro e Farinha", address: "Rua Voluntários da Pátria, 259", category: "italian", phone_number: "2124-6722")
+restaurant1 = Restaurant.create(name: "Gurumê", address: "Rua Pacheco Leão,40", category: "japonese", phone_number: "3495-7882")
+puts "------------ Seed completed ----------"
